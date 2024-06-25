fb138de5a380927bb43dcb44ee8289745d497d48
Tom.L <tom.louveau@edu.devinci.fr>
2024-06-25 17:06:06 +0200

    ADD movies list and navigate to list

--- projects/movies/src/app/features/movies/components/child-edit-movie/child-edit-movie.component.ts
+++ projects/movies/src/app/features/movies/components/child-edit-movie/child-edit-movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Movie } from '../../models';
 
@@ -17,6 +17,7 @@ export class ChildEditMovieComponent {
   });
 
   requestToSave = output<Movie>();
+  buttonSaveTitle = input.required<string>();
 
   saveToParent(): void {
     this.requestToSave.emit(this.movieForm.value as Movie);
